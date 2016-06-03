51a6088cf0afb11e6265600004755bc64d3bb664
gao <465716992@qq.com>
2016-06-03 16:17:54 +0800

    local

---
+++ wifi/js/messages_cn.js
@@ -0,0 +1,25 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Language: CN
+ * Author: Fayland Lam <fayland at gmail dot com>
+ */
+jQuery.extend(jQuery.validator.messages, {
+        required: "该字段必须填写",
+		remote: "请修正该字段",
+		email: "请输入正确格式的电子邮件",
+		url: "请输入合法的网址",
+		date: "请输入合法的日期",
+		dateISO: "请输入合法的日期 (ISO).",
+		number: "请输入合法的数字",
+		digits: "只能输入整数",
+		creditcard: "请输入合法的信用卡号",
+		equalTo: "请再次输入相同的值",
+		accept: "请输入拥有合法后缀名的字符串",
+		maxlength: jQuery.validator.format("您输入长度不能超过{0}位"),
+		minlength: jQuery.validator.format("您输入长度不能少于{0}位"),
+		rangelength: jQuery.validator.format("您输入长度介于{0}至{1}位之间"),
+		range: jQuery.validator.format("您输入整数大小介于{0}和{1}之间"),
+		max: jQuery.validator.format("您输入数值最大不能超过{0}"),
+		min: jQuery.validator.format("您输入数值最小不能低于{0}")
+	
+});
